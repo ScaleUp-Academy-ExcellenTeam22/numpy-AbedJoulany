aeda4507108ddae0227c6d4b0993e5da30d41c87
AbedJoulany <59060418+AbedJoulany@users.noreply.github.com>
2022-06-01 19:58:39 +0300

    Delete question1.py

--- question1.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-
-if __name__ == '__main__':
-    arr = np.arange(0, 21)
-    print("vector before signing: ", arr)
-    arr[9:16] *= -1
-    print("vector after signing: ", arr)
